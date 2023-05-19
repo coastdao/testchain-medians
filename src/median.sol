47bcf993f3658ee0a0d8363d718f1a2654978ada
fx0x55 <80245546+fx0x55@users.noreply.github.com>
2023-05-19 13:58:46 +0800

    add WETHUSD median

--- src/median.sol
+++ src/median.sol
@@ -30,6 +30,17 @@ contract MedianETHUSD is Median {
     }
 }
 
+contract MedianWETHUSD is Median {
+    bytes32 public constant wat = "WETHUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
+
 contract MedianFXUSD is Median {
     bytes32 public constant wat = "FXUSD";
 
