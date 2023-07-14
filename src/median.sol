4dac1e7f628b39d35dc7c2c676aec3f11c81ca5b
fx0x55 <80245546+fx0x55@users.noreply.github.com>
2023-07-14 10:54:32 +0800

    add STFXUSD median

--- src/median.sol
+++ src/median.sol
@@ -51,3 +51,14 @@ contract MedianFXUSD is Median {
         );
     }
 }
+
+contract MedianSTFXUSD is Median {
+    bytes32 public constant wat = "STFXUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
