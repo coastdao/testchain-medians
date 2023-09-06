dcc96933ba710e87b766cfd81525cfbc73eead02
todd <81545601+todd-woko@users.noreply.github.com>
2023-09-06 18:53:24 +0800

    fix: MediantATOMtOSMOLPUSD, MedianUSDCtOSMOLPUSD, MedianWETHtOSMOLPUSD, MedianWBTCtOSMOLPUSD, MedianstOSMOtOSMOLPUSD

--- src/median.sol
+++ src/median.sol
@@ -73,3 +73,58 @@ contract MedianATOMOSMOLPUSD is Median {
         );
     }
 }
+
+contract MediantATOMtOSMOLPUSD is Median {
+    bytes32 public constant wat = "tATOMtOSMOLPUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
+
+contract MedianUSDCtOSMOLPUSD is Median {
+    bytes32 public constant wat = "USDCtOSMOLPUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
+
+contract MedianWETHtOSMOLPUSD is Median {
+    bytes32 public constant wat = "WETHtOSMOLPUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
+
+contract MedianWBTCtOSMOLPUSD is Median {
+    bytes32 public constant wat = "WBTCtOSMOLPUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
+
+contract MedianstOSMOtOSMOLPUSD is Median {
+    bytes32 public constant wat = "stOSMOtOSMOLPUSD";
+
+    function recover(uint256 val_, uint256 age_, uint8 v, bytes32 r, bytes32 s) internal pure returns (address) {
+        return ecrecover(
+            keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", keccak256(abi.encodePacked(val_, age_, wat)))),
+            v, r, s
+        );
+    }
+}
